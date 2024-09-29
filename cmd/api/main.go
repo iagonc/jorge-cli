cabc749fcab5dd844ac5d539da1331f50bee2263
iagonc <iago.neves@gmail.com>
2024-09-28 23:43:43 -0300

    fix: rollback wrong change in api main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,27 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/iagonc/jorge-cli/cmd/cli/commands"
-	"github.com/spf13/cobra"
+	"github.com/iagonc/jorge-cli/internal/config"
+	"github.com/iagonc/jorge-cli/internal/handler"
+	"github.com/iagonc/jorge-cli/internal/repository"
+	"github.com/iagonc/jorge-cli/internal/router"
+	"github.com/iagonc/jorge-cli/internal/usecase"
+	"go.uber.org/zap"
 )
 
 func main() {
-    var rootCmd = &cobra.Command{
-        Use:   "cli",
-        Short: "CLI to interact with the API",
-        Long:  "A command line tool to interact with the API for managing resources.",
-    }
+    // Initialize logger
+    logger, _ := zap.NewProduction()
+    defer logger.Sync()
 
-    rootCmd.AddCommand(commands.NewListCommand())
-    rootCmd.AddCommand(commands.NewCreateCommand())
-    rootCmd.AddCommand(commands.NewDeleteCommand())
-    rootCmd.AddCommand(commands.NewUpdateCommand())
-
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
+    // Initialize SQLite database
+    db, err := config.InitializeSQLite()
+    if err != nil {
+        logger.Sugar().Errorf("Failed to initialize SQLite: %v", err)
     }
+    repo := repository.NewSQLiteResourceRepository(db)
+
+    // Initialize use cases with the repository and logger
+    createUseCase := usecase.NewCreateResource(repo, logger)
+    deleteUseCase := usecase.NewDeleteResource(repo, logger)
+    getResourceByIDUseCase := usecase.NewGetResourceByID(repo, logger)
+    listUseCase := usecase.NewListResources(repo, logger)
+    listByNameUseCase := usecase.NewListResourcesByName(repo, logger)
+    updateUseCase := usecase.NewUpdateResource(repo, logger)
+
+    // Initialize handler with the use cases and logger
+    h := handler.NewHandler(
+        createUseCase,
+        deleteUseCase,
+        getResourceByIDUseCase,
+        listUseCase,
+        listByNameUseCase,
+        updateUseCase,
+        logger,
+    )
+
+    // Pass the handler to the router and initialize routes
+    router.Initialize(h)
 }
